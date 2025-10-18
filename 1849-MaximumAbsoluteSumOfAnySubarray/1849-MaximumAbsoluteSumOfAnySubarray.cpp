9ed8ca5c4991fb62a89b53f44affadb06334ed8a
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:01:33 +0530

    Create 1849-MaximumAbsoluteSumOfAnySubarray

---
+++ 1849-MaximumAbsoluteSumOfAnySubarray/1849-MaximumAbsoluteSumOfAnySubarray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 18/10/2025, 09:01:31
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int maxSum = 0, minSum = 0;
+        int currMax = 0, currMin = 0;
+
+        for (int num : nums) {
+            currMax = max(num, currMax + num);
+            maxSum = max(maxSum, currMax);
+            
+            currMin = min(num, currMin + num);
+            minSum = min(minSum, currMin);
+        }
+
+        return max(abs(maxSum), abs(minSum));
+    }
+};
