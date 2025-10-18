4da4fb9d945a9f2b3723edf1cf9757a0240b2033
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:00:13 +0530

    Create 2488-DivideIntervalsIntoMinimumNumberOfGroups

---
+++ 2488-DivideIntervalsIntoMinimumNumberOfGroups/2488-DivideIntervalsIntoMinimumNumberOfGroups.cpp
@@ -0,0 +1,20 @@
+// Last updated: 18/10/2025, 09:00:11
+class Solution {
+ public:
+  // Similar to 253. Meeting Rooms II
+  int minGroups(vector<vector<int>>& intervals) {
+    // Stores `right`s.
+    priority_queue<int, vector<int>, greater<>> minHeap;
+
+    ranges::sort(intervals);
+
+    for (const vector<int>& interval : intervals) {
+      // There's no overlap, so we can reuse the same group.
+      if (!minHeap.empty() && interval[0] > minHeap.top())
+        minHeap.pop();
+      minHeap.push(interval[1]);
+    }
+
+    return minHeap.size();
+  }
+};
