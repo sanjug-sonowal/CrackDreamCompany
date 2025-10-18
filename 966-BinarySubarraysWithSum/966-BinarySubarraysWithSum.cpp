1a363528a943ba71f7b5bbc772864a349217aaa9
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:04:21 +0530

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.cpp
@@ -0,0 +1,19 @@
+// Last updated: 18/10/2025, 09:04:19
+class Solution {
+ public:
+  int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int ans = 0;
+    int prefix = 0;
+    // {prefix: number of occurrence}
+    unordered_map<int, int> count{{0, 1}};
+
+    for (const int num : nums) {
+      prefix += num;
+      if (const auto it = count.find(prefix - goal); it != count.cend())
+        ans += it->second;
+      ++count[prefix];
+    }
+
+    return ans;
+  }
+};
