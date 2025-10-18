7a5cba223cf0f0c5a1f9b0728d72a64df161200a
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:00:40 +0530

    Create 2271-RearrangeArrayElementsBySign

---
+++ 2271-RearrangeArrayElementsBySign/2271-RearrangeArrayElementsBySign.cpp
@@ -0,0 +1,19 @@
+// Last updated: 18/10/2025, 09:00:38
+class Solution {
+ public:
+  vector<int> rearrangeArray(vector<int>& nums) {
+    vector<int> ans;
+    vector<int> pos;
+    vector<int> neg;
+
+    for (const int num : nums)
+      (num > 0 ? pos : neg).push_back(num);
+
+    for (int i = 0; i < pos.size(); ++i) {
+      ans.push_back(pos[i]);
+      ans.push_back(neg[i]);
+    }
+
+    return ans;
+  }
+};
