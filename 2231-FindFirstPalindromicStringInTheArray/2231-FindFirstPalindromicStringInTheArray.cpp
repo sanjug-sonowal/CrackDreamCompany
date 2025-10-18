8955e755f5d8ddc050ffc1f922598ba42366e197
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:00:45 +0530

    Create 2231-FindFirstPalindromicStringInTheArray

---
+++ 2231-FindFirstPalindromicStringInTheArray/2231-FindFirstPalindromicStringInTheArray.cpp
@@ -0,0 +1,20 @@
+// Last updated: 18/10/2025, 09:00:43
+class Solution {
+ public:
+  string firstPalindrome(vector<string>& words) {
+    for (const string& word : words)
+      if (isPalindrome(word))
+        return word;
+    return "";
+  }
+
+ private:
+  bool isPalindrome(const string& s) {
+    int i = 0;
+    int j = s.length() - 1;
+    while (i < j)
+      if (s[i++] != s[j--])
+        return false;
+    return true;
+  }
+};
