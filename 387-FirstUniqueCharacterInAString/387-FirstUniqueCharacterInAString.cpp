6977393731fe71a12e0ef600b7d2331722d61ce6
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:05:55 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,16 @@
+// Last updated: 18/10/2025, 09:05:53
+class Solution {
+ public:
+  int firstUniqChar(string s) {
+    vector<int> count(26);
+
+    for (const char c : s)
+      ++count[c - 'a'];
+
+    for (int i = 0; i < s.length(); ++i)
+      if (count[s[i] - 'a'] == 1)
+        return i;
+
+    return -1;
+  }
+};
