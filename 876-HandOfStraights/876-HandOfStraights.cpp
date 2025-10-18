e30ca838a5c595a91ee444e5fbb154ada6ec87e1
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:04:40 +0530

    Create 876-HandOfStraights

---
+++ 876-HandOfStraights/876-HandOfStraights.cpp
@@ -0,0 +1,22 @@
+// Last updated: 18/10/2025, 09:04:38
+class Solution {
+ public:
+  bool isNStraightHand(vector<int>& hand, int groupSize) {
+    map<int, int> count;
+
+    for (const int card : hand)
+      ++count[card];
+
+    for (const auto& [start, _] : count) {
+      const int value = count[start];
+      if (value > 0)
+        for (int i = start; i < start + groupSize; ++i) {
+          count[i] -= value;
+          if (count[i] < 0)
+            return false;
+        }
+    }
+
+    return true;
+  }
+};
