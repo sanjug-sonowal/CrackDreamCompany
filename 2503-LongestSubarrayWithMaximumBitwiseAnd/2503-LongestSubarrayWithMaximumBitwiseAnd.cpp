7f9a8c00756181b26d8b9384e05f3b55775ac5d8
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:00:09 +0530

    Create 2503-LongestSubarrayWithMaximumBitwiseAnd

---
+++ 2503-LongestSubarrayWithMaximumBitwiseAnd/2503-LongestSubarrayWithMaximumBitwiseAnd.cpp
@@ -0,0 +1,22 @@
+// Last updated: 18/10/2025, 09:00:07
+class Solution {
+ public:
+  int longestSubarray(vector<int>& nums) {
+    int ans = 0;
+    int maxIndex = 0;
+    int sameNumLength = 0;
+
+    for (int i = 0; i < nums.size(); ++i)
+      if (nums[i] == nums[maxIndex]) {
+        ans = max(ans, ++sameNumLength);
+      } else if (nums[i] > nums[maxIndex]) {
+        maxIndex = i;
+        sameNumLength = 1;
+        ans = 1;
+      } else {
+        sameNumLength = 0;
+      }
+
+    return ans;
+  }
+};
