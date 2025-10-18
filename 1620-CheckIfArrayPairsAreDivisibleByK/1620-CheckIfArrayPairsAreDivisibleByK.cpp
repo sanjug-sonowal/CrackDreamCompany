97d24fbf60231a9481cfb66a3ddedcd57b0eeff4
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:02:12 +0530

    Create 1620-CheckIfArrayPairsAreDivisibleByK

---
+++ 1620-CheckIfArrayPairsAreDivisibleByK/1620-CheckIfArrayPairsAreDivisibleByK.cpp
@@ -0,0 +1,21 @@
+// Last updated: 18/10/2025, 09:02:10
+class Solution {
+ public:
+  bool canArrange(vector<int>& arr, int k) {
+    vector<int> count(k);
+
+    for (int a : arr) {
+      a %= k;
+      ++count[a < 0 ? a + k : a];
+    }
+
+    if (count[0] % 2 != 0)
+      return false;
+
+    for (int i = 1; i <= k / 2; ++i)
+      if (count[i] != count[k - i])
+        return false;
+
+    return true;
+  }
+};
