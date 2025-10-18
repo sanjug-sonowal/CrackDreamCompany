11a7201f0fe780e2f93b8e2522e11d117edf66d2
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:07:42 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -0,0 +1,19 @@
+// Last updated: 18/10/2025, 09:07:40
+class Solution {
+ public:
+  vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> ans;
+    unordered_map<string, vector<string>> keyToAnagrams;
+
+    for (const string& str : strs) {
+      string key = str;
+      ranges::sort(key);
+      keyToAnagrams[key].push_back(str);
+    }
+
+    for (const auto& [_, anagrams] : keyToAnagrams)
+      ans.push_back(anagrams);
+
+    return ans;
+  }
+};
