b9f39b8e77012b848dc99e615e06d8196567ad02
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:02:56 +0530

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ 1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.cpp
@@ -0,0 +1,22 @@
+// Last updated: 18/10/2025, 09:02:54
+class Solution {
+public:
+    int checkLRS(string &s, string &s2 ,int i, int j, vector<vector<int>> &dp) {
+        if (i == 0 || j == 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (s[i - 1] == s2[j - 1])
+            return dp[i][j] = 1 + checkLRS(s, s2,i - 1, j - 1, dp);
+        else
+            return dp[i][j] = max(checkLRS(s,s2, i - 1, j, dp), checkLRS(s,s2, i, j - 1, dp));
+    }
+    int minInsertions(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+        int lps = checkLRS(s,s2, n, n, dp);
+        return n - lps;
+    }
+};
