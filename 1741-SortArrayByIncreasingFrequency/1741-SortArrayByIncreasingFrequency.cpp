55577e00088caa9b6e215b9ef07b66fd94c6be6f
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:01:51 +0530

    Create 1741-SortArrayByIncreasingFrequency

---
+++ 1741-SortArrayByIncreasingFrequency/1741-SortArrayByIncreasingFrequency.cpp
@@ -0,0 +1,20 @@
+// Last updated: 18/10/2025, 09:01:49
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int>freq;
+
+        for(int x:nums){
+            freq[x]++;
+        }
+
+        sort(nums.begin(),nums.end(),[&](int a,int b){
+            if(freq[a] == freq[b])
+            return a > b;
+
+            return freq[a] < freq[b];
+        });
+
+        return nums;
+    }
+};
