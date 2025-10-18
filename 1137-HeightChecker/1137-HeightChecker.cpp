b90d5a33a32b779a676f077721e7fe2c202ffd97
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:03:46 +0530

    Create 1137-HeightChecker

---
+++ 1137-HeightChecker/1137-HeightChecker.cpp
@@ -0,0 +1,22 @@
+// Last updated: 18/10/2025, 09:03:45
+class Solution {
+ public:
+  int heightChecker(vector<int>& heights) {
+    int ans = 0;
+    int currentHeight = 1;
+    vector<int> count(101);
+
+    for (int height : heights)
+      ++count[height];
+
+    for (int height : heights) {
+      while (count[currentHeight] == 0)
+        ++currentHeight;
+      if (height != currentHeight)
+        ++ans;
+      --count[currentHeight];
+    }
+
+    return ans;
+  }
+};
