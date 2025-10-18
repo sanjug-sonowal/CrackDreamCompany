a9e295a025d7d76a9c643c8d10c8df7ab9f790bf
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:01:55 +0530

    Create 1730-SpecialArrayWithXElementsGreaterThanOrEqualX

---
+++ 1730-SpecialArrayWithXElementsGreaterThanOrEqualX/1730-SpecialArrayWithXElementsGreaterThanOrEqualX.cpp
@@ -0,0 +1,18 @@
+// Last updated: 18/10/2025, 09:01:53
+class Solution {
+ public:
+  int specialArray(vector<int>& nums) {
+    ranges::sort(nums);
+
+    if (nums[0] >= nums.size())
+      return nums.size();
+
+    for (int i = 1; i < nums.size(); ++i) {
+      const int count = nums.size() - i;
+      if (nums[i - 1] < count && nums[i] >= count)
+        return count;
+    }
+
+    return -1;
+  }
+};
