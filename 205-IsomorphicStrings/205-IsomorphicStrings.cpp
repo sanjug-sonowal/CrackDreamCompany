7260cca779c6267a644865cfe5857036093cbd40
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:06:45 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -0,0 +1,17 @@
+// Last updated: 18/10/2025, 09:06:43
+class Solution {
+ public:
+  bool isIsomorphic(string s, string t) {
+    vector<int> charToIndex_s(128);
+    vector<int> charToIndex_t(128);
+
+    for (int i = 0; i < s.length(); ++i) {
+      if (charToIndex_s[s[i]] != charToIndex_t[t[i]])
+        return false;
+      charToIndex_s[s[i]] = i + 1;
+      charToIndex_t[t[i]] = i + 1;
+    }
+
+    return true;
+  }
+};
