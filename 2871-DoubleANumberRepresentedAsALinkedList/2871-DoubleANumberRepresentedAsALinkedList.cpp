fbdafe6bb1ee42925a9a2f7fcaa6cb1aa28f1695
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 08:59:28 +0530

    Create 2871-DoubleANumberRepresentedAsALinkedList

---
+++ 2871-DoubleANumberRepresentedAsALinkedList/2871-DoubleANumberRepresentedAsALinkedList.cpp
@@ -0,0 +1,29 @@
+// Last updated: 18/10/2025, 08:59:26
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* doubleIt(ListNode* head) {
+    if (getCarry(head) == 1)
+      return new ListNode(1, head);
+    return head;
+  }
+
+ private:
+  int getCarry(ListNode* node) {
+    int val = node->val * 2;
+    if (node->next != nullptr)
+      val += getCarry(node->next);
+
+    node->val = val % 10;
+    return val / 10;
+  }
+};
