ed45fedf36240abac0e22d74744b0bded443ee28
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:06:08 +0530

    Create 330-PatchingArray

---
+++ 330-PatchingArray/330-PatchingArray.cpp
@@ -0,0 +1,21 @@
+// Last updated: 18/10/2025, 09:06:06
+class Solution {
+ public:
+  int minPatches(vector<int>& nums, int n) {
+    int ans = 0;
+    int i = 0;      // nums' index
+    long miss = 1;  // the minimum sum in [1, n] we might miss
+
+    while (miss <= n)
+      if (i < nums.size() && nums[i] <= miss) {
+        miss += nums[i++];
+      } else {
+        // Greedily add `miss` itself to increase the range from
+        // [1, miss) to [1, 2 * miss).
+        miss += miss;
+        ++ans;
+      }
+
+    return ans;
+  }
+};
