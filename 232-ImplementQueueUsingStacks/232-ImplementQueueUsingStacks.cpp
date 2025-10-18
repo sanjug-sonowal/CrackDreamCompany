a6c590b4451796d27d626a1b70dfa6e3b9195e7d
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:06:33 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.cpp
@@ -0,0 +1,29 @@
+// Last updated: 18/10/2025, 09:06:31
+class MyQueue {
+ public:
+  void push(int x) {
+    input.push(x);
+  }
+
+  int pop() {
+    peek();
+    const int val = output.top();
+    output.pop();
+    return val;
+  }
+
+  int peek() {
+    if (output.empty())
+      while (!input.empty())
+        output.push(input.top()), input.pop();
+    return output.top();
+  }
+
+  bool empty() {
+    return input.empty() && output.empty();
+  }
+
+ private:
+  stack<int> input;
+  stack<int> output;
+};
