1954c9119875f0e2872b34897f5d583851d56d36
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:07:38 +0530

    Create 54-SpiralMatrix

---
+++ 54-SpiralMatrix/54-SpiralMatrix.cpp
@@ -0,0 +1,48 @@
+// Last updated: 18/10/2025, 09:07:36
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int row = matrix.size(),col = matrix[0].size();
+        int top = 0,bottom = row - 1,right = col - 1,left = 0;
+
+        while(left <= right && top <= bottom)
+        {
+            //print top
+            for(int j = left;j <= right;j++)
+            {
+                ans.push_back(matrix[top][j]);
+             
+            }
+               top++;
+            //print right
+            for(int i = top;i <= bottom;i++)
+            {
+                ans.push_back(matrix[i][right]);
+                
+            }
+            right--;
+            //print bottom
+            if(top <= bottom)
+            {
+                for(int j = right;j >= left;j--)
+                {
+                    ans.push_back(matrix[bottom][j]);
+                 
+                }
+                   bottom--;
+            }
+            //print left
+            if(left <= right)
+            {
+                for(int i = bottom;i >= top;i--)
+                {
+                    ans.push_back(matrix[i][left]);
+                
+                }
+                    left++;
+            }
+        }
+        return ans;
+    }
+};
