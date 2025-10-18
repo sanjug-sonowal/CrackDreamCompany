3c42a8f6cf7a4dc0cbfd1dc4d2bea38fbec63d78
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:05:39 +0530

    Create 452-MinimumNumberOfArrowsToBurstBalloons

---
+++ 452-MinimumNumberOfArrowsToBurstBalloons/452-MinimumNumberOfArrowsToBurstBalloons.cpp
@@ -0,0 +1,19 @@
+// Last updated: 18/10/2025, 09:05:37
+class Solution {
+ public:
+  int findMinArrowShots(vector<vector<int>>& points) {
+    ranges::sort(points,
+                 [](const auto& a, const auto& b) { return a[1] < b[1]; });
+
+    int ans = 1;
+    int arrowX = points[0][1];
+
+    for (int i = 1; i < points.size(); ++i)
+      if (points[i][0] > arrowX) {
+        arrowX = points[i][1];
+        ++ans;
+      }
+
+    return ans;
+  }
+};
