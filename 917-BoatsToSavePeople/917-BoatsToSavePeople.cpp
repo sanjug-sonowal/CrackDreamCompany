416017e9ed86cb65013190d6b5b505f64496168f
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:04:29 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.cpp
@@ -0,0 +1,17 @@
+// Last updated: 18/10/2025, 09:04:27
+class Solution {
+ public:
+  int numRescueBoats(vector<int>& people, int limit) {
+    int ans = 0;
+
+    ranges::sort(people);
+
+    for (int i = 0, j = people.size() - 1; i <= j; ++ans) {
+      const int remain = limit - people[j--];
+      if (people[i] <= remain)
+        ++i;
+    }
+
+    return ans;
+  }
+};
