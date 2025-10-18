cb597b4fa023aeae3adbbdf243299a0e8e4477d0
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:00:43 +0530

    Create 2255-MinimumSwapsToGroupAll1sTogetherIi

---
+++ 2255-MinimumSwapsToGroupAll1sTogetherIi/2255-MinimumSwapsToGroupAll1sTogetherIi.cpp
@@ -0,0 +1,20 @@
+// Last updated: 18/10/2025, 09:00:41
+class Solution {
+ public:
+  int minSwaps(vector<int>& nums) {
+    const int n = nums.size();
+    const int k = ranges::count(nums, 1);
+    int ones = 0;     // the number of ones in the window
+    int maxOnes = 0;  // the maximum number of ones in the window
+
+    for (int i = 0; i < n * 2; ++i) {
+      if (i >= k && nums[(i - k) % n])
+        --ones;
+      if (nums[i % n])
+        ++ones;
+      maxOnes = max(maxOnes, ones);
+    }
+
+    return k - maxOnes;
+  }
+};
