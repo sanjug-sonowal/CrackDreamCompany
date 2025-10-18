5edb32510fd1c78079e884736ff69f62cc3c257c
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 08:59:08 +0530

    Create 3307-FindTheMaximumSumOfNodeValues

---
+++ 3307-FindTheMaximumSumOfNodeValues/3307-FindTheMaximumSumOfNodeValues.cpp
@@ -0,0 +1,20 @@
+// Last updated: 18/10/2025, 08:59:06
+class Solution {
+ public:
+  long long maximumValueSum(vector<int>& nums, int k,
+                            vector<vector<int>>& edges) {
+    long long maxSum = 0;
+    int changedCount = 0;
+    int minChangeDiff = INT_MAX;
+
+    for (const int num : nums) {
+      maxSum += max(num, num ^ k);
+      changedCount += ((num ^ k) > num) ? 1 : 0;
+      minChangeDiff = min(minChangeDiff, abs(num - (num ^ k)));
+    }
+
+    if (changedCount % 2 == 0)
+      return maxSum;
+    return maxSum - minChangeDiff;
+  }
+};
