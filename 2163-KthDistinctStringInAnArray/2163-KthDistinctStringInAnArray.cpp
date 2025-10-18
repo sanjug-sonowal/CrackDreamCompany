5a4f2984c21268f32bbbcdc7131c3a435b1ba213
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:00:55 +0530

    Create 2163-KthDistinctStringInAnArray

---
+++ 2163-KthDistinctStringInAnArray/2163-KthDistinctStringInAnArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 18/10/2025, 09:00:54
+class Solution {
+ public:
+  string kthDistinct(vector<string>& arr, int k) {
+    unordered_map<string, int> count;
+
+    for (const string& a : arr)
+      ++count[a];
+
+    for (const string& a : arr)
+      if (count[a] == 1 && --k == 0)
+        return a;
+
+    return "";
+  }
+};
