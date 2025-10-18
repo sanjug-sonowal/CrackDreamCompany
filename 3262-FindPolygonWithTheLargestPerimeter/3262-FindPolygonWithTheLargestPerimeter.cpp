51ad7e19d9a981b03a12f145e91a5adeab10a536
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 08:59:11 +0530

    Create 3262-FindPolygonWithTheLargestPerimeter

---
+++ 3262-FindPolygonWithTheLargestPerimeter/3262-FindPolygonWithTheLargestPerimeter.cpp
@@ -0,0 +1,19 @@
+// Last updated: 18/10/2025, 08:59:09
+class Solution {
+ public:
+  long long largestPerimeter(vector<int>& nums) {
+    long long prefix = accumulate(nums.begin(), nums.end(), 0LL);
+
+    ranges::sort(nums);
+
+    for (int i = nums.size() - 1; i >= 2; --i) {
+      prefix -= nums[i];
+      // Let nums[i] be the longest side. Check if the sum of all the edges with
+      // length no longer than nums[i] > nums[i].
+      if (prefix > nums[i])
+        return prefix + nums[i];
+    }
+
+    return -1;
+  }
+};
