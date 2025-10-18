99b0374bb762ff0ae64f72f76de6e39aeee830c8
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:01:41 +0530

    Create 1802-NumberOfStudentsUnableToEatLunch

---
+++ 1802-NumberOfStudentsUnableToEatLunch/1802-NumberOfStudentsUnableToEatLunch.cpp
@@ -0,0 +1,18 @@
+// Last updated: 18/10/2025, 09:01:40
+class Solution {
+ public:
+  int countStudents(vector<int>& students, vector<int>& sandwiches) {
+    vector<int> count(2);
+
+    for (const int student : students)
+      ++count[student];
+
+    for (int i = 0; i < sandwiches.size(); ++i) {
+      if (count[sandwiches[i]] == 0)
+        return sandwiches.size() - i;
+      --count[sandwiches[i]];
+    }
+
+    return 0;
+  }
+};
