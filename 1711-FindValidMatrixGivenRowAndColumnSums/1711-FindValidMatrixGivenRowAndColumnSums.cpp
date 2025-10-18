85aaacd3ddf6ee200e449cffa3f0dbe514278e95
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:01:57 +0530

    Create 1711-FindValidMatrixGivenRowAndColumnSums

---
+++ 1711-FindValidMatrixGivenRowAndColumnSums/1711-FindValidMatrixGivenRowAndColumnSums.cpp
@@ -0,0 +1,18 @@
+// Last updated: 18/10/2025, 09:01:55
+class Solution {
+ public:
+  vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+    const int m = rowSum.size();
+    const int n = colSum.size();
+    vector<vector<int>> ans(m, vector<int>(n));
+
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j) {
+        ans[i][j] = min(rowSum[i], colSum[j]);
+        rowSum[i] -= ans[i][j];
+        colSum[j] -= ans[i][j];
+      }
+
+    return ans;
+  }
+};
