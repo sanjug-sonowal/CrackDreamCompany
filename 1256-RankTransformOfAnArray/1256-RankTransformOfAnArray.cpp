effc125d8582584f90c4b08a2f01359581402667
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 09:03:29 +0530

    Create 1256-RankTransformOfAnArray

---
+++ 1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.cpp
@@ -0,0 +1,19 @@
+// Last updated: 18/10/2025, 09:03:27
+class Solution {
+ public:
+  vector<int> arrayRankTransform(vector<int>& arr) {
+    vector<int> sortedArr(arr);
+    unordered_map<int, int> rank;
+
+    ranges::sort(sortedArr);
+
+    for (const int a : sortedArr)
+      if (!rank.contains(a))
+        rank[a] = rank.size() + 1;
+
+    for (int& a : arr)
+      a = rank[a];
+
+    return arr;
+  }
+};
