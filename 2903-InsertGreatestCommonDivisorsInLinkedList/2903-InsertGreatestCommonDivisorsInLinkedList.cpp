fab99770270b21a82572c711ce3dc3d2dc786704
SANJUG <68654609+sanjug-sonowal@users.noreply.github.com>
2025-10-18 08:59:26 +0530

    Create 2903-InsertGreatestCommonDivisorsInLinkedList

---
+++ 2903-InsertGreatestCommonDivisorsInLinkedList/2903-InsertGreatestCommonDivisorsInLinkedList.cpp
@@ -0,0 +1,23 @@
+// Last updated: 18/10/2025, 08:59:24
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* insertGreatestCommonDivisors(ListNode* head) {
+    for (ListNode* curr = head; curr->next != nullptr;) {
+      ListNode* inserted =
+          new ListNode(__gcd(curr->val, curr->next->val), curr->next);
+      curr->next = inserted;
+      curr = inserted->next;
+    }
+    return head;
+  }
+};
